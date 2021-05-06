4bc33e3e03bae0d30823c4d9048dcb65dca40b43
Ollie Nichols <ollienichols22@gmail.com>
2021-05-06 21:55:14 +0000

    First commit

---
+++ programs/python2.py
@@ -0,0 +1,83 @@
+# <QUESTION 1>
+
+# Given a list of items, return a dictionary mapping items to the amount
+# of times they appear in the list
+
+# Note: the order of this dictionary does not matter
+
+# <EXAMPLES>
+
+# one(['apple', 'banana', 'orange', 'orange', 'apple', 'apple']) → {'apple':3, 'orange':2, 'banana':1}
+# one(['tic', 'tac', 'toe']) → {'tic':1, 'tac':1, 'toe':1}
+    
+def one(items):
+    pass
+
+# <QUESTION 2>
+
+# Given two numbers, a & b, and an operator, evaluate the operation between a & b
+# using the given operator
+
+# The operator will be a string, and will only be +, -, *, or /. 
+
+# <EXAMPLES>
+
+# two(2, 4, '+') → 6
+# two(7, 3, '-') → 4
+# two(3, 1.5, '*') → 4.5
+# two(-5, 2, '/') → -2.5
+
+def two(a, b, operator):
+    pass
+
+# <QUESTION 3>
+
+# Given a positive integer, return the next integer below it that has an
+# integer square root (is the square of another integer)
+
+# If the number has a square root, just return the number
+
+# <EXAMPLES>
+
+# three(7) → 4          # 4 is the square of 2
+# three(64) → 64        # 64 is the square of 8 already
+# three(32) → 25
+
+# <HINT>
+
+# We can use `x ** 0.5` to get the square root of `x`
+
+def three(num):
+    pass
+
+# <QUESTION 4>
+
+# Given two integers, return the greatest common factor of the two numbers
+
+# <EXAMPLES>
+
+# four(32, 24) → 8
+# four(18, 11) → 1
+# four(10, 50) → 10
+
+def four(a, b):
+    pass
+
+# <QUESTION 5>
+
+# Given a string, return a string where each letter is the previous letter in the alphabet
+# in comparison to the original string
+
+# For a or A, use z or Z respectively
+
+# Ignore punctuation or whitespace
+
+# <EXAMPLES>
+
+# five('wxyz') → 'vwxy'
+# five('abc') → 'zab'
+# five('aAbB') → 'zZaA'
+# five('hello world') == ''
+
+def five(string):
+    pass
---
+++ tests/test_assessment.py
@@ -0,0 +1,31 @@
+from programs import python2
+
+def test_one():
+    assert python2.one(['apple', 'banana', 'orange', 'orange', 'apple', 'apple']) == {'apple':3, 'orange':2, 'banana':1}
+    assert python2.one(['tic', 'tac', 'toe']) == {'tic':1, 'tac':1, 'toe':1}
+    assert python2.one([]) == {}
+    assert python2.one(['bert']*1000) == {'bert':1000}
+
+def test_two():
+    for a, b in [ (5, 3), (3, 1.5), (-5, 7) , (0, 1) ]:
+        for operator in ['+', '-', '*', '/']:
+            assert python2.two(a, b, operator) == eval(f'{a} {operator} {b}')
+
+def test_three():
+    assert python2.three(7) == 4
+    assert python2.three(64) == 64
+    assert python2.three(32) == 25
+    assert python2.three(1) == python2.three(2) == 1
+
+def test_four():
+    assert python2.four(32, 24) == 8
+    assert python2.four(18, 11) == 1
+    assert python2.four(10, 50) == 10
+    assert python2.four(1, 1) == 1
+    assert python2.four(2, 2048) == 2
+
+def test_five():
+    assert python2.five('wxyz') == 'vwyz'
+    assert python2.five('abc') == 'zab'
+    assert python2.five('aAbB') == 'zZaA'
+    assert python2.five('abcabcABCABC') == 'zabzabZABZAB'
